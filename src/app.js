65c5037ff93958133e91600113d6054e68a6d32a
Luis Gabriel Rodriguez <luisgabriel84@gmail.com>
2019-07-09 10:33:14 -0500

    fixing routes

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ app.use((req, res, next)=>{
     .use(express.static(boweComponents))
     
     .get('/',(req, res)=>{
-        res.render('users/login')
+        res.render('./users/login')
     })
     
 
